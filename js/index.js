7c688920b0f867af2ef0cba36b73efb089a5c863
Omar Abdullah <36677597+omarabdullah1@users.noreply.github.com>
2024-06-26 20:55:59 +0300

    cc
    safsag

--- js/index.js
+++ js/index.js
@@ -1,3 +1,26 @@
+ 
+// sImport the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC7gn3OzFXaanP1g4dJBI2304tgzRReqGE",
+  authDomain: "cc-k-base-2a7f8.firebaseapp.com",
+  databaseURL: "https://cc-k-base-2a7f8-default-rtdb.firebaseio.com",
+  projectId: "cc-k-base-2a7f8",
+  storageBucket: "cc-k-base-2a7f8.appspot.com",
+  messagingSenderId: "743977287743",
+  appId: "1:743977287743:web:139787dd57cf8b19fdf88a",
+  measurementId: "G-5Z9Z6TLL1W"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 document.getElementById('sparePartsForm').addEventListener('submit', function(event) {
     event.preventDefault();
